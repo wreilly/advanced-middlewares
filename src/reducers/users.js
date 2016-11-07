3829f42e3c4e871746d181125ab6341488b235c0
William Reilly <wreilly2001@gmail.com>
2016-11-07 04:50:46 -0500

    Question (answered) re: spread operator on nested array. Bon.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -12,6 +12,19 @@ export default function(state = [], action) {
 action.payload
 Promise__proto__: Promise[[PromiseStatus]]: "pending"[[PromiseValue]]: undefined
     */
+    /* WR__ Question:
+    A. Think I found my answer, asking my question.
+    Yes, if you don't do the '...' you wind up with
+    [array01, [array02]]
+    whereas what you want is
+    [array01, array02[0], array02[1]].
+    Cheers.
+    Q. Why does action.payload need the spread operator?
+    What precisely is in that 'action.payload'?
+    Isn't it an [array] ? [{user1},{user2}] ?
+    To put an [array01] inside another [array02], is spread operator necessary ? [array02, [array01]]
+    */
+      // return [ ...state, action.payload ]; // Nope. (Secondo me.)
       return [ ...state, ...action.payload ];
   }
   return state;
